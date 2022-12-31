9ccdda887765399ca16d2138c6ac753c75f3504e
Truong Tuan Loc <Loctruongtuan@gmail.com>
2022-12-31 17:03:59 +0700

    app

--- app/App e-Signature/Form1.Designer.cs
+++ app/App e-Signature/Form1.Designer.cs
@@ -29,27 +29,38 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button6 = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(613, 140);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(204, 72);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Tải khóa công khai";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // tabControl1
             // 
@@ -77,7 +88,7 @@
             this.tabPage1.Controls.Add(this.textBox2);
             this.tabPage1.Controls.Add(this.textBox1);
             this.tabPage1.Controls.Add(this.button3);
-            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.Button2);
             this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Location = new System.Drawing.Point(4, 29);
             this.tabPage1.Name = "tabPage1";
@@ -87,75 +98,58 @@
             this.tabPage1.Text = "RSA";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // button7
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 29);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(792, 417);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "ElGamal";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(613, 271);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(204, 148);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Xác thực File";
+            this.button7.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // button5
             // 
-            this.button1.Location = new System.Drawing.Point(473, 49);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Upload File";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button5.Location = new System.Drawing.Point(473, 390);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 29);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Upload Key";
+            this.button5.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // button4
             // 
-            this.button2.Location = new System.Drawing.Point(613, 49);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(204, 72);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Mã hóa File và tải xuống";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button4.Location = new System.Drawing.Point(473, 332);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Upload File";
+            this.button4.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // textBox4
             // 
-            this.button3.Location = new System.Drawing.Point(473, 269);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(94, 29);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Upload File";
-            this.button3.UseVisualStyleBackColor = true;
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(22, 390);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(430, 27);
+            this.textBox4.TabIndex = 10;
             // 
-            // textBox1
+            // textBox3
             // 
-            this.textBox1.Location = new System.Drawing.Point(22, 49);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(430, 27);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(22, 333);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(430, 27);
+            this.textBox3.TabIndex = 9;
             // 
-            // textBox2
+            // label4
             // 
-            this.textBox2.Location = new System.Drawing.Point(22, 271);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(430, 27);
-            this.textBox2.TabIndex = 4;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 26);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(161, 20);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Tải lên File cần mã hóa\r\n";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 248);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(166, 20);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Tải lên File cần xác thực";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 367);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(231, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Tải lên khóa công khai nhận được";
             // 
             // label3
             // 
@@ -167,64 +161,79 @@
             this.label3.Text = "Tải lên File mã hóa nhận được";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
-            // label4
+            // label2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(22, 367);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(231, 20);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Tải lên khóa công khai nhận được";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Tải lên File cần xác thực";
             // 
-            // textBox3
+            // label1
             // 
-            this.textBox3.Location = new System.Drawing.Point(22, 333);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(430, 27);
-            this.textBox3.TabIndex = 9;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Tải lên File cần mã hóa\r\n";
             // 
-            // textBox4
+            // textBox2
             // 
-            this.textBox4.Location = new System.Drawing.Point(22, 390);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(430, 27);
-            this.textBox4.TabIndex = 10;
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(22, 271);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(430, 27);
+            this.textBox2.TabIndex = 4;
             // 
-            // button4
+            // textBox1
             // 
-            this.button4.Location = new System.Drawing.Point(473, 332);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(94, 29);
-            this.button4.TabIndex = 11;
-            this.button4.Text = "Upload File";
-            this.button4.UseVisualStyleBackColor = true;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(22, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(430, 27);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // button5
+            // button3
             // 
-            this.button5.Location = new System.Drawing.Point(473, 390);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(94, 29);
-            this.button5.TabIndex = 12;
-            this.button5.Text = "Upload Key";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button3.Location = new System.Drawing.Point(473, 269);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Upload File";
+            this.button3.UseVisualStyleBackColor = true;
             // 
-            // button6
+            // Button2
             // 
-            this.button6.Location = new System.Drawing.Point(613, 140);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(204, 72);
-            this.button6.TabIndex = 13;
-            this.button6.Text = "Tải khóa công khai";
-            this.button6.UseVisualStyleBackColor = true;
+            this.Button2.Location = new System.Drawing.Point(613, 49);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(204, 72);
+            this.Button2.TabIndex = 1;
+            this.Button2.Text = "Mã hóa File và tải xuống";
+            this.Button2.UseVisualStyleBackColor = true;
+            this.Button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button7
+            // button1
             // 
-            this.button7.Location = new System.Drawing.Point(613, 271);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(204, 148);
-            this.button7.TabIndex = 14;
-            this.button7.Text = "Xác thực File";
-            this.button7.UseVisualStyleBackColor = true;
+            this.button1.Location = new System.Drawing.Point(473, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Upload File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(871, 464);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "ElGamal";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -250,7 +259,7 @@
         private TextBox textBox2;
         private TextBox textBox1;
         private Button button3;
-        private Button button2;
+        private Button Button2;
         private Button button1;
         private Label label3;
         private Label label2;
--- app/App e-Signature/Form1.cs
+++ app/App e-Signature/Form1.cs
@@ -1,3 +1,12 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Net.Security;
+
 namespace App_e_Signature
 {
     public partial class Form1 : Form
@@ -6,6 +15,58 @@ namespace App_e_Signature
         {
             InitializeComponent();
         }
+        #region RSA
+
+        int RSA_soP, RSA_soQ, RSA_soN, RSA_soE, RSA_soD, RSA_soPhi_n;
+        public int RSA_d_dau = 0;
+        private int RSA_ChonSoNgauNhien()
+        {
+            Random rd = new Random();
+            return rd.Next(11, 101);
+        }
+        private bool RSA_kiemTraNguyenTo(int xi)
+        {
+            bool kiemtra = true;
+            if (xi == 2 || xi == 3)
+            {
+                // kiemtra = true;
+                return kiemtra;
+            }
+            else
+            {
+                if (xi == 1 || xi % 2 == 0 || xi % 3 == 0)
+                {
+                    kiemtra = false;
+                }
+                else
+                {
+                    for (int i = 5; i <= Math.Sqrt(xi); i = i + 6)
+                        if (xi % i == 0 || xi % (i + 2) == 0)
+                        {
+                            kiemtra = false;
+                            break;
+                        }
+                }
+            }
+            return kiemtra;
+        }
+        // "Hàm kiểm tra hai số nguyên tố cùng nhau"
+        private bool RSA_nguyenToCungNhau(int ai, int bi)
+        {
+            bool ktx_;
+            // giải thuật Euclid;
+            int temp;
+            while (bi != 0)
+            {
+                temp = ai % bi;
+                ai = bi;
+                bi = temp;
+            }
+            if (ai == 1) { ktx_ = true; }
+            else ktx_ = false;
+            return ktx_;
+        }
+        #endregion
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
@@ -20,6 +81,27 @@ namespace App_e_Signature
         private void label3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+    
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string plaintext = File.ReadAllText(textBox1.Text);
+
+
+        }
+   
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = file.FileName;
+            }
         }
     }
 }
