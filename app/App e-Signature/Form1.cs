e733a2a352e5d5f9d28b596860e215b50d66a3a6
Truong Tuan Loc <Loctruongtuan@gmail.com>
2022-12-31 15:34:34 +0700

    app

--- app/App e-Signature/Form1.cs
+++ app/App e-Signature/Form1.cs
@@ -6,5 +6,20 @@ namespace App_e_Signature
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
