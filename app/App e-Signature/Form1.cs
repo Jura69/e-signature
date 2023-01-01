b7200838da5da8e835c717e95a8554235de689d7
Truong Tuan Loc <Loctruongtuan@gmail.com>
2023-01-01 13:20:23 +0700

    app

--- app/App e-Signature/Form1.cs
+++ app/App e-Signature/Form1.cs
@@ -66,6 +66,58 @@ namespace App_e_Signature
             else ktx_ = false;
             return ktx_;
         }
+        public int RSA_mod(int mx, int ex, int nx)
+        {
+
+            //Sử dụng thuật toán "bình phương nhân"
+            //Chuyển e sang hệ nhị phân
+            int[] a = new int[100];
+            int k = 0;
+            do
+            {
+                a[k] = ex % 2;
+                k++;
+                ex = ex / 2;
+            }
+            while (ex != 0);
+            //Quá trình lấy dư
+            int kq = 1;
+            for (int i = k - 1; i >= 0; i--)
+            {
+                kq = (kq * kq) % nx;
+                if (a[i] == 1)
+                    kq = (kq * mx) % nx;
+            }
+            return kq;
+        }
+
+        private void RSA_taoKhoa()
+        {
+            //Tinh n=p*q
+            RSA_soN = RSA_soP * RSA_soQ;
+            rsa_soN.Text = RSA_soN.ToString();
+            //Tính Phi(n)=(p-1)*(q-1)
+            RSA_soPhi_n = (RSA_soP - 1) * (RSA_soQ - 1);
+            rsa_soPhiN.Text = RSA_soPhi_n.ToString();
+            //Tính e là một số ngẫu nhiên có giá trị 0< e <phi(n) và là số nguyên tố cùng nhau với Phi(n)
+            do
+            {
+                Random RSA_rd = new Random();
+                RSA_soE = RSA_rd.Next(2, RSA_soPhi_n);
+            }
+            while (!nguyenToCungNhau(RSA_soE, RSA_soPhi_n));
+            rsa_soE.Text = RSA_soE.ToString();
+
+            //Tính d là nghịch đảo modular của e
+            RSA_soD = 0;
+            int i = 2;
+            while (((1 + i * RSA_soPhi_n) % RSA_soE) != 0 || RSA_soD <= 0)
+            {
+                i++;
+                RSA_soD = (1 + i * RSA_soPhi_n) / RSA_soE;
+            }
+            rsa_soD.Text = RSA_soD.ToString();
+        }
         #endregion
 
         private void textBox1_TextChanged(object sender, EventArgs e)
